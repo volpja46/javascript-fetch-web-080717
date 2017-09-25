b14ae7c9e4d0738c9f2a38dac6882378d6c66fbf
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-09-25 08:58:06 -0400

    :fetch:

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much.";
+fetch('https://api.github.com/volpja46/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
+
+
+  const token = '175819156faa2dc037497c20c553211df84e3073'
+  fetch('https://api.github.com/volpja46/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
